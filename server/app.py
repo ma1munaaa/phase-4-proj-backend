375f18c8db709e965e3ea8f2b506e5e7207b7b4d
Eric <ericmuthuipatch22@gmail.com>
2023-10-15 16:53:21 +0300

    cors

--- server/app.py
+++ server/app.py
@@ -30,7 +30,11 @@ db.init_app(app)
 jwt = JWTManager(app)
 
 
-CORS(app, supports_credentials=True)
+# CORS(app, supports_credentials=True)
+
+cors = CORS(app, resources={r"/api/*": {"origins": "https://phase-4-proj-frontend-iv-c6o5.vercel.app"}})
+
+
 
 # CORS(app, resources={r"/*": {"origins": "https://phase-4-proj-frontend-iv-c6o5.vercel.app/"}}, supports_credentials=True)
 
